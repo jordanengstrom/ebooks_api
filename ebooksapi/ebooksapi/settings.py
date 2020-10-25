7a2d8bd765bfcce82ea81f134e441f3d8e51d11c
Jordan Engstrom <jordan.engstrom@gmail.com>
2020-10-25 16:32:19 -0600

    added pagination

--- ebooksapi/ebooksapi/settings.py
+++ ebooksapi/ebooksapi/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # REST_FRAMEWORK = {
-#     'DEFUALT_PERMISSION_CLASSES': (
-#         'rest_framework.permissions.IsAuthenticated',
-#     )
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 3
 # }
